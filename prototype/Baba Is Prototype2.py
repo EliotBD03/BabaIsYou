08990376d565a27524a280c47879672cdd7e3260
ArnaudIsYou <99490306+ArnaudIsYou@users.noreply.github.com>
2022-02-12 21:29:37 +0100

    Version améliorée du prototype 2
    J'ai séparé le code du générateur de niveau en plusieurs fonctions et j'utilise plus des variables pour réduire le temps de génération d'un niveau et augmenter la propreté du code en général

--- prototype/Baba Is Prototype2.py
+++ prototype/Baba Is Prototype2.py
@@ -2,22 +2,29 @@ import random
 
 def generate_level(length, height):
     generated_level = []
-    temp = []
-    for i in range(length):
-        temp.append("x")
-    bordure_NS = temp
-    generated_level.append(bordure_NS)
-    while len(generated_level) < height - 1:
-        temp = []
-        for i in range(length):
-            if len(temp) == 0 or len(temp) == length - 1:
-                temp.append("x")
-                if len(temp) == length:
-                    generated_level.append(temp)
-            else:
-                temp.append(" ")
-    generated_level.append(bordure_NS)
+    bordures_NS = generate_bordures(length, height, generated_level)
+    bordures_OE = generate_corps(length, height, generated_level)
+    generated_level.append(bordures_NS)
+    for i in range(height - 1):
+        generated_level.append(bordures_OE)
+    generated_level.append(bordures_NS)
     return generated_level
+
+def generate_bordures(length, height, generated_level):
+    bordure_NS = []
+    for i in range(length):
+        bordure_NS.append("x")
+    return bordure_NS
+
+def generate_corps(length, height, generated_level):
+    bordure_OE = ["x"]
+    for i in range(length):
+        if len(bordure_OE) == length - 1:
+            bordure_OE.append("x")
+        else:
+            bordure_OE.append(" ")
+    return bordure_OE
+
 
 def print_level(level):
     for j in level:
