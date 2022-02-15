2c0560f1e3682ed4303e51c9e29fb71203ccbd84
EliotBD03 <89980155+EliotBD03@users.noreply.github.com>
2022-02-15 09:15:18 +0100

    correction mur + ajout flag

--- prototype/BabaIsYouJulien2.py
+++ prototype/BabaIsYouJulien2.py
@@ -9,7 +9,7 @@ class Environment(list):
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
-                            ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
+                            ['x',' ',' ','@',' ',' ',' ',' ',' ','x'],
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
@@ -17,46 +17,79 @@ class Environment(list):
                             ['x',' ',' ',' ',' ',' ',' ',' ',' ','x'],
                             ['x','x','x','x','x','x','x','x','x','x']]
     def __str__(self):
-    #on peut afficher la carte    
+    #ce qui nous permet d'afficher la carte    
         res = ''
         for line in self.environment:
             res += ' '.join(line) + '\n'
         return res    
 
-class Wall(Environment):
+class Wall(object):
 
     def __init__(self):
+    #initialisation de notre mur
         self.wall = 'x'
 
-    def stop(self,position1,position2):
+    def isstopped(self,position1,position2):
     #délimiter la carte    
-        if not(Environment().environment[position1][position2] == self.wall):
-            return True
-        else:
-            return False    
+        if Environment().environment[position1][position2] == self.wall:
+            return False
+        return True 
 
-
-class Player(Wall):
+class Goal(object):
 
     def __init__(self):
-    #coordonnés de notre joueur + toutes les positions possibles après mouvement
+    #initialisation de notre objectif
+        self.goal = '@'  
+
+    def isreached(self, position1, position2):
+    #nous permet de savoir si on a atteint l'objectif
+        if Environment().environment[position1][position2] == self.goal:
+            return True
+        return False              
+
+
+class Player(object):
+
+    def __init__(self):
+    #coordonnés de notre joueur (par défaut : milieu de la carte)
         self.x = (len(Environment().environment[0]) - 1) // 2
         self.y = (len(Environment().environment) - 1) // 2
-        self.xleft = self.x - 1
-        self.xright = self.x + 1
-        self.yup = self.y - 1
-        self.ydown = self.y + 1
-
     def move(self, direction):
-    #ce qui permet de faire bouger le joueur
-        if direction == 'z' and Wall().stop(self.yup, self.x):
-            return self.x ,self.y - 1 
-        elif direction == 's' and Wall().stop(self.yup,self.x):
-            return self.x ,self.y + 1
-        elif direction == 'q' and Wall().stop(self.y,self.xleft):
-            return self.x - 1 ,self.y
-        elif direction == 'd' and Wall().stop(self.y,self.xright):
-            return self.x + 1 , self.y    
+    #ce qui permet de faire bouger le joue et de savoir si on a atteint l'objectif
+        if direction == 'z' and Wall().isstopped(self.y - 1, self.x):
+
+            if Goal().isreached(self.y - 1, self.x):
+                print("You have reached the goal. You WON !")
+                return True
+             
+            return self.y -1 ,self.x 
+        elif direction == 's' and Wall().isstopped(self.y + 1,self.x):
+            
+            if Goal().isreached(self.y + 1, self.x):
+                print("You have reached the goal. You WON !")
+                return True
+            
+            return self.y + 1, self.x
+        elif direction == 'q' and Wall().isstopped(self.y,self.x - 1):
+            
+            if Goal().isreached(self.y, self.x - 1):
+                print("You have reached the goal. You WON !")
+                return True
+        
+            return self.y, self.x - 1
+        elif direction == 'd' and Wall().isstopped(self.y,self.x + 1):
+            
+            if Goal().isreached(self.y, self.x + 1):
+                print("You have reached the goal. You WON !")
+                return True    
+            
+            return self.y, self.x + 1    
+        else:
+            return self.y , self.x
+    """
+    on évalue la position relative du joueur après mouvement pour voir si le joueur
+    ne retrouvera pas dans un mur
+    """  
 
 
 
@@ -69,8 +102,13 @@ if __name__ == "__main__":
         temp = Environment()
         temp.environment[player.y][player.x]= player_skin
         print(temp)
-        intp = str(input("z : up q : left s : down d : right"))
-        player.x , player.y = player.move(intp)
+        intp = str(input("z : up q : left s : down d : right "))
+
+        if type(player.move(intp)) == tuple:
+            player.y , player.x = player.move(intp)
+        else:
+            break    
+        print("\t" *  1000)
 
 
     """
