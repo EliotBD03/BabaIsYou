d0e1b6ed2f1ff2d04b5257f7a16d5f12c96282a9
Julien <julienladeuze@outlook.fr>
2022-03-10 21:01:27 +0100

    algo extract fait, possible d'avoir une meilleure complexité

--- extracteur de donné/Extract.java
+++ extracteur de donné/Extract.java
@@ -3,7 +3,7 @@
  */
 import java.io.BufferedReader;
 /**
- * permet de savoir si on peut lire le fichier 
+ * permet de savoir si on peut lire le fichier
  */
 import java.io.FileReader;
 /**
@@ -12,46 +12,67 @@ import java.io.FileReader;
 import java.io.IOException;
 /**
  * cette classe a pour but d'extraire les donnees d'un fichier texte
- * cette classe ne comporte que des méthodes 
  */
 
 public class Extract
 {
-    /**
-     * ne permet pour l'instant que d'utiliser un fichier et le lire 
-     */
-    public static void extract(){
+    private String[][] dataList;
 
+    public String[][] getDataList()
+    {
+        if (this.dataList != null)
+            return this.dataList;
+        return null;
+    }
+
+    /**
+     * on extrait les donnés de notre fichier pour les mettre sous la forme d'un tableau a deux dimensions
+     * @param map le fichier texte representant la map du jeu
+     */
+    public void setDataList(String map){
         try
         {
-            BufferedReader r1 = new BufferedReader(new FileReader("test.txt"));
+            //on effectue les intanciations necessaires
+            FileReader file = new FileReader(map); BufferedReader r1 = new BufferedReader(file);
             String line;
+
+            //on evalue le nombre de ligne pour notre tableau à deux dimensions
             int i = 0;
             while((line = r1.readLine()) != null)
                 ++i;
-            String[] res = new String[i];
-            i = 0;
+
+            //on instancie ce le tableau de String
+            String[][] res = new String[i][3];
             r1.close();
-            BufferedReader r2 = new BufferedReader(new FileReader("test.txt"));
-            while((line = r2.readLine()) != null){
-                res[i] = line;
-                ++i;
+
+            //on instancie une deuxieme fois pour "réouvrir" le fichier
+            FileReader file2 = new FileReader(map); BufferedReader r2 = new BufferedReader(file2);
+
+            int k = 0; // la ligne de res
+            //tant qu'on a pas atteint la fin de notre fichier
+            while((line = r2.readLine()) != null)
+            {
+                int j = 0; // evaluer la longueur de la chaine de la ligne "line" de notre fichier map
+                int l = 0; //la colonne de res
+                res[k][0] = ""; res[k][1] = ""; res[k][2] = ""; // on efface les valeurs null dans notre tableau
+                //tant qu'on a pas atteint la fin de la ligne ET que l'indice de notre sous tableau <= 2
+                while(j <= line.length() - 1 && l <= 2)
+                {
+                    res[k][l] += line.charAt(j);
+                    if(line.charAt(j) == ' ')
+                        l++;
+                    j++;
+                }
+                k++;
             }
             r2.close();
-            for(i = 0; i <= res.length - 1; ++i)
-                System.out.println(res[i]);
-
+            // on met notre attribut de classe comme etant le tableau res (pour pouvoir le manipuler par la suite)
+            this.dataList = res;
         }
         catch(IOException error)
         {
             error.printStackTrace();
         }
-    } 
-
-    public static void main(String[] arg)
-    {
-        extract();
     }
 }
 
-// en attente du type de retour 
