ad3e494ac2c1ff62a154ef3c7f1cd2b86ab87fe7
Julien <julienladeuze@outlook.fr>
2022-03-31 08:39:36 +0200

    systeme de regle opérationnel mais peut être amelioré

--- projet/main/java codeJ/BigAlgorithm.java
+++ projet/main/java codeJ/BigAlgorithm.java
@@ -2,11 +2,10 @@
  * cette classe permet de savoir pour quel item du jeu ,on y retrouve sa règle
  */
 
-public class BigAlgorithm extends Map
-{
+public class BigAlgorithm extends Map {
     public static Enum[][] tabloc = new Enum[getLength()][getWidth()];
-    public static Enum[][] tabperm;
-    private Enum[] character = {Rules.BABA,Rules.ROCK, Rules.FLAG, Rules.WALL};
+    private static Enum[][] tabperm;
+    private Enum[] character = {Rules.BABA, Rules.ROCK, Rules.FLAG, Rules.WALL};
     private Enum[] actions = {Rules.YOU, Rules.STOP, Rules.WIN, Rules.PUSH};
 
 
@@ -14,52 +13,69 @@ public class BigAlgorithm extends Map
      * constructeur permettant de ne executer les methodes à chaques fois
      */
 
-    public BigAlgorithm()
-    {
+    public BigAlgorithm() {
         setTab(super.mapO);
-        findPerm(findIS());
+        int[][] current_is = findIS();
+        setTabperm(findPermX(current_is), findPermY(current_is));
     }
 
     /**
      * accesseur pour le tableau des permissions
+     *
      * @return tableau à 2 dimensions
      */
-    public static Enum[][] getTabperm(){return tabperm;}
+    public static Enum[][] getTabperm() {
+        return tabperm;
+    }
 
     /**
      * cree notre tableau "tabloc" a deux dimensions regroupant les regles (ex: BabaIsYou)
+     *
      * @param map0 le tableau issue de map representant les objets
      */
 
-    public void setTab(Entity[][] map0)
-    {
+    public void setTab(Entity[][] map0) {
         for (int i = 1; i <= map0.length - 1; i++)
-            for(int j = 1; j <= map0[i].length - 1; j++)
-            if(map0[i][j] != null)
-            {
-                //System.out.println("on passe la condition");
-                tabloc[i][j] = whatobj(map0[i][j].getClass());
-            }
-            else
-                tabloc[i][j] = Rules.NONE;
+            for (int j = 1; j <= map0[i].length - 1; j++)
+                if (map0[i][j] != null) {
+                    //System.out.println("on passe la condition");
+                    tabloc[i][j] = whatobj(map0[i][j].getClass());
+                } else
+                    tabloc[i][j] = Rules.NONE;
+    }
+    public void setTabperm(Enum[][] tabpermX, Enum[][] tabpermY)
+    {
+        tabperm = new Enum[20][2];
+        int i = 0;
+        for(Enum[] elemX : tabpermX)
+        {
+            tabperm[i][0] = elemX[0]; tabperm[i][1] = elemX[1];
+            i++;
+        }
+        for(Enum[] elemY : tabpermY)
+        {
+            tabperm[i][0] = elemY[0]; tabperm[i][1] = elemY[1];
+            i++;
+        }
     }
 
     /**
      * cherche dans le tableau "tabloc" les "Is" permettant de faire l'union de deux objets (ex: Baba et You)
+     *
      * @return un tableau à deux dimensions regroupant les Is et leurs positions de cette maniere:
      * un sous tableau = Is , les elements de Is => position en y et en x (dans cet ordre)
      */
 
-    public int[][] findIS()
-    {
-        int x = -1; int y = -1;
+    public int[][] findIS() {
+        int x = -1;
+        int y = -1;
         int[][] res = new int[10][2];
         int k = 0;
-        for(int i = 0; i <= tabloc.length - 1; i++)
-            for(int j = 0 ; j <= tabloc[i].length - 1; j++)
-                if(tabloc[i][j] == Rules.IS)
-                {
-                    res[k][0] = i ; res[k][1] = j;
+        for (int i = 0; i <= tabloc.length - 1; i++)
+            for (int j = 0; j <= tabloc[i].length - 1; j++)
+                if (tabloc[i][j] == Rules.IS) {
+                    res[k][0] = i;
+                    res[k][1] = j;
                     k++;
                 }
         tabperm = new Enum[res.length * 2][2];
@@ -68,109 +84,112 @@ public class BigAlgorithm extends Map
 
     /**
      * cree un tableau à double dimensions representant l'objet (ex: Baba) et sa regle (ex: You)
+     *
      * @param isLoc le tableau cree par la methode "findIs()"
      */
 
-    public void findPerm(int[][] isLoc)
-    {
-
+    public Enum[][] findPermY(int[][] isLoc) {
+        Enum[][] tabpermY = new Enum[10][2];
         int k = 0;
-        int l = 0;
-        for(int[] is : isLoc)
+        for (int[] is : isLoc)
         {
             if (is[0] > 0)
-                {
-                    //on cherche s'il n'y a pas un "personnage" (ex : baba, flag, ect) au dessus du is
-                    for (int i = 0; i <= character.length - 1; i++)
-                    {
-                        if (tabloc[is[0] - 1][is[1]] == character[i])
-                        {
-                            tabperm[k][0] = character[i];
-                            System.out.println("perso : " + tabperm[k][0] + "pos : " + k + " " + 0);
-                            k++;
-                        }
-                    }
-                }
-
-            if (is[1] > 0)
-                {
-                    //on cherche s'il n' y a pas un "personnage" (ex: baba, flag, wall, ect) à gauche du is
-                    for (int i = 0; i <= character.length - 1; i++)
-                    {
-                        if (tabloc[is[0]][is[1] - 1] == character[i])
-                        {
-                            tabperm[k][0] = character[i];
-                            System.out.println("perso : " + tabperm[k][0] + "pos : " + k + " " + 0);
-                            k++;
-                        }
-                    }
-                }
-
-            if(is[0] < tabloc[0].length - 1)
             {
-                // on cherche s'il n 'y a pas une action (ex : you, win, ect) en dessous du is
-                for (int j = 0; j <= actions.length - 1; j++)
+                //on cherche s'il n'y a pas un "personnage" (ex : baba, flag, ect) au dessus du is
+                for (int i = 0; i <= character.length - 1; i++)
                 {
-                    if (tabloc[is[0] + 1][is[1]] == actions[j] && tabperm[l][0] != null)
+                    if (tabloc[is[0] - 1][is[1]] == character[i])
                     {
-                        tabperm[l][1] = actions[j];
-                        System.out.println("action : " + tabperm[l][1] + "pos : " + l + " " + 1);
-                        l++;
+                        tabpermY[k][0] = character[i];
+                        System.out.println("perso : " + tabpermY[k][0] + " pos : " + k + " " + 0 + " instruc 1");
                     }
                 }
+
             }
 
-            if(is[1] < tabloc[0].length - 1)
+            if (tabpermY[k][0] != null)
             {
-                // on cherche s'il n 'y a pas une action (ex : you, win, ect) à droite du is
-                for(int j = 0; j <= actions.length - 1; j++)
+                if (is[0] < tabloc[0].length - 1)
                 {
-                    if(tabloc[is[0]][is[1] + 1] == actions[j] && tabperm[l][0] != null)
+                    // on cherche s'il n 'y a pas une action (ex : you, win, ect) en dessous du is
+                    for (int j = 0; j <= actions.length - 1; j++)
                     {
-                        tabperm[l][1] = actions[j];
-                        System.out.println("action : " + tabperm[l][1] + "pos : " + l + " " + 1);
-                        l++;
+                        if (tabloc[is[0] + 1][is[1]] == actions[j])
+                        {
+                            tabpermY[k][1] = actions[j];
+                            System.out.println("action : " + tabpermY[k][1] + " pos : " + k + " " + 1 + " instruc 3");
+                            k++;
+                        }
                     }
                 }
             }
         }
-        for(int i = 0; i <= tabperm.length - 1; i++)
-            System.out.println(tabperm[i][0] + " " + tabperm[i][1]);
+        return tabpermY;
+    }
+
+    public Enum[][] findPermX(int[][] isLoc)
+    {
+        Enum[][] tabpermX = new Enum[10][2];
+        int k = 0;
+        for (int[] is : isLoc)
+        {
+            if (is[1] > 0)
+            {
+                //on cherche s'il n' y a pas un "personnage" (ex: baba, flag, wall, ect) à gauche du is
+                for (int i = 0; i <= character.length - 1; i++)
+                {
+                    if (tabloc[is[0]][is[1] - 1] == character[i])
+                    {
+                        tabpermX[k][0] = character[i];
+                        System.out.println("perso : " + tabpermX[k][0] + " pos : " + k + " " + 0 + " instruc 2");
+                    }
+                }
+            }
+            if (tabpermX[k][0] != null)
+            {
+                if (is[1] < tabloc[0].length - 1)
+                {
+                    // on cherche s'il n 'y a pas une action (ex : you, win, ect) à droite du is
+                    for (int j = 0; j <= actions.length - 1; j++)
+                    {
+                        if (tabloc[is[0]][is[1] + 1] == actions[j])
+                        {
+                            tabpermX[k][1] = actions[j];
+                            System.out.println("action : " + tabpermX[k][1] + " pos : " + k + " " + 1 + " instruc 4");
+                            k++;
+                        }
+                    }
+                }
+            }
+        }
+        return tabpermX;
     }
 
     /**
      * permet de savoir quel objet ou regle se trouve en fonction du String issu du fichier texte "level"
+     *
      * @param thing l'element du fichier texte representant un objet ou une regle
      * @return l'objet de type "Rules" (voir Rules.java) representant un objet ou une regle
      */
 
-    private Enum whatobj(Class<?> thing)
-    {
+    private Enum whatobj(Class<?> thing) {
         if (Is.class.equals(thing)) {
             return Rules.IS;
-        }
-        else if (Stop.class.equals(thing)) {
+        } else if (Stop.class.equals(thing)) {
             return Rules.STOP;
-        }
-        else if (Push.class.equals(thing)) {
+        } else if (Push.class.equals(thing)) {
             return Rules.PUSH;
-        }
-        else if (TextWall.class.equals(thing)) {
+        } else if (TextWall.class.equals(thing)) {
             return Rules.WALL;
-        }
-        else if (TextRock.class.equals(thing)) {
+        } else if (TextRock.class.equals(thing)) {
             return Rules.ROCK;
-        }
-        else if (TextBaba.class.equals(thing)) {
+        } else if (TextBaba.class.equals(thing)) {
             return Rules.BABA;
-        }
-        else if (TextFlag.class.equals(thing)) {
+        } else if (TextFlag.class.equals(thing)) {
             return Rules.FLAG;
-        }
-        else if (You.class.equals(thing)) {
+        } else if (You.class.equals(thing)) {
             return Rules.YOU;
-        }
-        else if (Win.class.equals(thing)) {
+        } else if (Win.class.equals(thing)) {
             return Rules.WIN;
         }
         return Rules.NONE;
@@ -179,17 +198,13 @@ public class BigAlgorithm extends Map
     /**
      * permet d'actualiser le tableau de permissions après changement de position d'une instance dans le tableau d'objet
      */
-    public void actualise()
-    {
+    public void actualise() {
         setTab(mapO);
-        findPerm(findIS());
+        int[][] current_is = findIS();
+        setTabperm(findPermX(current_is), findPermY(current_is));
         int i = 0;
-        while(tabperm[i][0] != null)
-        {
-            System.out.println(tabperm[i][0] + " " + tabperm[i][1]);
-            i++;
-        }
     }
+}
 
 /*
     public static void actualiseX(Enum thing, int x)
@@ -248,4 +263,3 @@ public class BigAlgorithm extends Map
      */
 
 
-}
