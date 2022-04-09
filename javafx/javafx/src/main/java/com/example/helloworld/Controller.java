fa62b4215965f390bde321bb433cd684888f8d6e
RomainELoy <100307603+RomainELoy@users.noreply.github.com>
2022-04-09 14:04:54 +0200

    Update Controller.java
    Ajout: des méthodes pour switch de scène avec ajout du keyevent car faut bien le mettre quelque part.

--- javafx/javafx/src/main/java/com/example/helloworld/Controller.java
+++ javafx/javafx/src/main/java/com/example/helloworld/Controller.java
@@ -1,35 +1,148 @@
 package com.example.helloworld;
+
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 
 public class Controller {
     @FXML
     private ImageView monImageView;
-    private double x = 278.0;
-    private double y = 165.0;
+    @FXML
+    private ImageView monImageView2;
+    private double x = 200.0;
+    private double y = 250.0;
+    private double x2 = 200.0;
+    private double y2 = 250.0;
+    @FXML
+    private Button logoutButton;
+    @FXML
+    private AnchorPane scenePane;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,450, 650 , Color.BLACK);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,450, 650 , Color.BLACK);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToGame(ActionEvent event) throws IOException {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+            Parent root = loader.load();
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Controller controller = loader.getController();
+            scene = new Scene(root,450,650,Color.BLACK);
+            stage.setScene(scene);
+            stage.show();
+
+            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) {
+                    switch (event.getCode()) {
+                        case UP:
+                            controller.up();
+                            break;
+                        case DOWN:
+                            controller.down();
+                            break;
+                        case LEFT:
+                            controller.left();
+                            break;
+                        case RIGHT:
+                            controller.right();
+                            break;
 
 
-    public void up()
-    {
-        System.out.println("UP");
-        monImageView.setLayoutY(y=y-50);
+                    }
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    public void down()
-    {
-        System.out.println("DOWN");
-        monImageView.setLayoutY(y=y+50);
+    public void logout(ActionEvent event){
+        stage = (Stage)scenePane.getScene().getWindow();
+        System.out.println("logout");
+        stage.close();
     }
-    public void left()
-    {
-        System.out.println("LEFT");
-        monImageView.setLayoutX(x=x-50);
+    public void up() {
+        monImageView.setLayoutY(y = y - 50);
+        monImageView2.setLayoutY(y2 = y2 - 50);
+        System.out.println("y" + y);
+        if (y < 100) {
+            System.out.println("bord de map en y:" + y);
+            monImageView.setLayoutY(y = 50);
+        }
+        if (y2 < 100) {
+            monImageView2.setLayoutY(y2 = 50);
+        }
     }
-    public void right()
-    {
-        System.out.println("RIGHT");
-        monImageView.setLayoutX(x=x+50);
+
+    public void down() {
+        monImageView.setLayoutY(y = y + 50);
+        monImageView2.setLayoutY(y2 = y2 + 50);
+        System.out.println("y" + y);
+        if (y > 550) {
+            System.out.println("bord de map en y:" + y);
+            monImageView.setLayoutY(y = 550);
+        }
+        if (y2 > 550) {
+            monImageView2.setLayoutY(y2 = 550);
+        }
+    }
+
+    public void left() {
+        monImageView.setLayoutX(x = x - 50);
+        monImageView2.setLayoutX(x2 = x2 - 50);
+        monImageView2.setOpacity(1);
+
+        System.out.println("x" + x);
+        if (x < 100) {
+            System.out.println("bord de map en x:" + x);
+            monImageView.setLayoutX(x = 50);
+        }
+        if (x2 < 100) {
+            monImageView2.setLayoutX(x2 = 50);
+        }
+    }
+
+    public void right() {
+        monImageView.setLayoutX(x = x + 50);
+        monImageView2.setLayoutX(x2 = x2 + 50);
+        monImageView2.setOpacity(0);
+        System.out.println("x" + x);
+        if (x > 350) {
+            System.out.println("bord de map en x:" + x);
+            monImageView.setLayoutX(x = 350);
+        }
+        if (x2 > 350) {
+            monImageView2.setLayoutX(x2 = 350);
+        }
     }
 }
