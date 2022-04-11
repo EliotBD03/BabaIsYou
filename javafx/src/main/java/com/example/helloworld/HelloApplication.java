65bcf14c929c77df7c35a0fe10258888cf32bc97
RomainELoy <100307603+RomainELoy@users.noreply.github.com>
2022-04-11 11:53:48 +0200

    Update HelloApplication.java

--- javafx/src/main/java/com/example/helloworld/HelloApplication.java
+++ javafx/src/main/java/com/example/helloworld/HelloApplication.java
@@ -3,49 +3,33 @@ package com.example.helloworld;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.input.KeyEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.io.IOException;
+
 
 
 public class HelloApplication extends Application {
 
     @Override
-    public void start(Stage stage) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root, Color.BLACK);
-            Controller controller = loader.getController();
+    public void start(Stage stage)  throws IOException {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+            Scene scene = new Scene(root,450, 650 , Color.BLACK);
+            stage.setTitle("Baba Is You");
             stage.setScene(scene);
             stage.show();
-            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-                @Override
-                public void handle(KeyEvent event) {
-                    switch(event.getCode()){
-                        case UP:controller.up();
-                        break;
-                        case DOWN:controller.down();
-                        break;
-                        case LEFT:controller.left();
-                            break;
-                        case RIGHT:controller.right();
-                            break;
-
-
-                    }
-                }
-            });
-
-        } catch (Exception e) {
+        } catch(Exception e){
             e.printStackTrace();
         }
 
     }
+
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+
 
