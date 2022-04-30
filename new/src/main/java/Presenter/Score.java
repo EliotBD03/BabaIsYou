c63b73109e6ba4fcbb3d685e49b4013a86d976a5
RomainELoy <100307603+RomainELoy@users.noreply.github.com>
2022-04-30 12:39:08 +0200

    ajout d'un chrono
    Ajout d'un chrono

--- new/src/main/java/Presenter/Score.java
+++ new/src/main/java/Presenter/Score.java
@@ -1,10 +1,26 @@
 package Presenter;
-
+import java.util.Timer;
+import java.util.TimerTask;
 /**
  * cette classe à pour but d'établir un tableau des scores
  */
 
 public class Score
 {
+int secondsPassed=0;
+Timer myTimer = new Timer();
+TimerTask task = new TimerTask(){
+    public void run(){
+        secondsPassed++;
+        System.out.println("Vous avez perdu: "+ secondsPassed+"0 points");
+    }
+};
+public void start(){
+    myTimer.scheduleAtFixedRate(task,1000,1000);
 
+}
+public static void main(String[] args){
+    Score score = new Score();
+    score.start();
+}
 }
