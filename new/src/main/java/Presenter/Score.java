c3578522c9795697e1a6be3a1fb37730e33c2237
RomainELoy <100307603+RomainELoy@users.noreply.github.com>
2022-05-02 18:19:18 +0200

    ajout: écrit dans le .text

--- new/src/main/java/Presenter/Score.java
+++ new/src/main/java/Presenter/Score.java
@@ -1,40 +1,112 @@
 package Presenter;
+
+import java.io.*;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Scanner;
-/**
- * cette classe à pour but d'établir un tableau des scores
- */
 
-public class Score
-{
-int secondsPassed = 0;
-int globalScore = 1500;
-Timer myTimer = new Timer();
-TimerTask task = new TimerTask(){
-    public void run(){
-        secondsPassed++;
-        System.out.println("Vous avez perdu: "+ secondsPassed+"0 points");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Avez vous finis ? si oui taper 1 sinon taper 2");
-        int entier = sc.nextInt();
-        if(entier == 1 | secondsPassed == 150){
-            stop();
-            cancel();
+/**
+ * cette classe est faite pour pouvoir accéder aux infos d'un fichier texte
+ */
+public class Score {
+    private String fileName;
+    private static int userLine = 0;
+    static int secondsPassed = 0;
+    static int globalScore = 0;
+    static int levelScore = 1500;
+    static Timer myTimer = new Timer();
+    TimerTask task = new TimerTask(){
+        public void run(){
+            secondsPassed++;
+            System.out.println("Vous avez perdu: "+ secondsPassed+"0 points");
+            Scanner sc = new Scanner(System.in);
+            if(secondsPassed == 150){
+                stop();
+            }
+        }
+    };
+
+    public Score() {
+
+    }
+
+    public static void stop(){
+        globalScore = globalScore + levelScore - secondsPassed*10;
+        System.out.println("Votre score: "+ globalScore+" points");
+        myTimer.cancel();
+    }
+
+    public void start(){
+        myTimer.scheduleAtFixedRate(task,1000,1000);
+
+    }
+
+    public Score(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+
+    public void writeInfo(String information)
+    {
+        try {
+
+            File file = new File(fileName);
+            if(file.exists())
+            {
+                FileWriter fw = new FileWriter(fileName, true);
+                fw.write("\n" + information);
+                fw.close();
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("cannot write in " + fileName);
+            e.printStackTrace();
         }
     }
-};
-public void stop(){
-globalScore = globalScore - secondsPassed*10;
-    System.out.println("Votre score: "+ globalScore+" points");
+
+    public String getUserInfo(String userId)
+    {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            String line = null;
+            String res = "";
+            int tempLine = 0;
+            while (scanner.hasNextLine())
+            {
+                tempLine += 1;
+                line = scanner.nextLine();
+                for(int i = 0; i <= line.length() - 1; i++)
+                {
+                    if(i <= userId.length() - 1 && line.charAt(i) != userId.charAt(i))
+                        break;
+                    else if(i > userId.length() - 1 && line.charAt(i) != ' ')
+                    {
+                        res += line.charAt(i);
+                        userLine = tempLine - 1;
+                    }
+                }
+            }
+            scanner.close();
+            if(res.equals(""))
+                return null;
+            return res;
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(fileName + " not found");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+        Info info = new Info("/Users/romaineloy/new/src/main/resources/level/save/history.txt");
+        System.out.println(info.getUserInfo("juju"));
+    }
+
 }
 
-public void start(){
-    myTimer.scheduleAtFixedRate(task,1000,1000);
-
-}
-public static void main(String[] args){
-    Score score = new Score();
-    score.start();
-}
-}
