5f79d1d69ed87de36b4aa6f2c51fd37e87bf9644
RomainELoy <100307603+RomainELoy@users.noreply.github.com>
2022-05-02 14:56:48 +0200

    Update Score.java

--- new/src/main/java/Presenter/Score.java
+++ new/src/main/java/Presenter/Score.java
@@ -7,14 +7,24 @@ import java.util.TimerTask;
 
 public class Score
 {
-int secondsPassed=0;
+int secondsPassed = 0;
+int globalScore = 1500;
 Timer myTimer = new Timer();
 TimerTask task = new TimerTask(){
     public void run(){
         secondsPassed++;
         System.out.println("Vous avez perdu: "+ secondsPassed+"0 points");
+        if(secondsPassed == 5){
+            stop();
+            cancel();
+        }
     }
 };
+public void stop(){
+globalScore = globalScore - secondsPassed*10;
+    System.out.println("Votre score: "+ globalScore+" points");
+}
+
 public void start(){
     myTimer.scheduleAtFixedRate(task,1000,1000);
 
